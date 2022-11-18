fa2b2c6c6eefecf72221ef823b5b18cf1003868f
ishimweeli <ishimweeli0@gmail.com>
2022-11-18 16:35:10 +0200

    profile

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">ISHIMWE ELIAB </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            from <span className="purple"> Kigali,RWANDA.</span>
             <br />I am a junior pursuing web developmet carrier.
             <br />
             <br />
